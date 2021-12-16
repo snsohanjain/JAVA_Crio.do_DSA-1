ceac53be8b896e992f66e0cea64d4331e6c49336
sohanjain <snsohanjain@gmail.com>
2021-12-16 15:02:55 +0530

    odd number commit

--- 15.Print odd number upto n/src/com/sohan/OddNumbers.java
+++ 15.Print odd number upto n/src/com/sohan/OddNumbers.java
@@ -19,6 +19,23 @@ public class OddNumbers {
     // TODO: Implement this method
     static ArrayList<Integer> oddNumbers(int n) {
 
+        // create a list
+        ArrayList<Integer> oddNumbersList = new ArrayList<>();
+        // iterate from 1 till n
+        for (int i = 1; i <=n; i++){
+            // check  if the number is divisible 2
+            if (i % 2 !=0) {
+                // if it is not then add into the list
+                oddNumbersList.add(i);
+            }
+            // return the list
+        } return oddNumbersList;
+
+
+
+
+
+
 
     }
 }
