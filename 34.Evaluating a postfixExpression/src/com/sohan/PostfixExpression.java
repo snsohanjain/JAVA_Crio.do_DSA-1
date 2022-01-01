809a344f4ada9cdc607826d95feae8b0f4b282e4
sohanjain <snsohanjain@gmail.com>
2022-01-01 10:40:23 +0530

    new one

--- 34.Evaluating a postfixExpression/src/com/sohan/PostfixExpression.java
+++ 34.Evaluating a postfixExpression/src/com/sohan/PostfixExpression.java
@@ -76,3 +76,31 @@ public class PostfixExpression {
         }return stack.pop();
     }
 }
+
+//    Problem Description
+//    Postfix expressions are an ordering of mathematical operations, where the operator(+,-,* etc) comes after the operands.
+//
+//        For instance, an operation 2 + 3 (infix notation, which humans use) will be represented as 2 3 + in postfix notation. The computers can understand postfix notation.
+//
+//        Your job is to evaluate the given postfix expression. The expression is given as a string where each token is separated by a space.
+//
+//        The possible operators can be +, - and *
+//
+//        Note that all operands(numbers) are assumed to be non-negative.
+//
+//        Input format
+//        One line of input, containing the string S.
+//
+//        Output format
+//        Print the result of the given expression
+//
+//        Sample Input 1
+//        2 3 +
+//
+//        Sample Output 1
+//        5
+//
+//        Constraints
+//        1 <= |S| <= 100
+//
+//        S[i] >= 0, where S[i] is an operand in the expression
